b6be5a86156bfdec19de5cf664e06682713862b1
Evan Turner <eturner@opentable.com>
2017-06-17 15:28:51 -0500

    ignore Data folder and adding an exploration file

---
+++ Code/exploration.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import requests
+import os
+import json
+import datetime
+
+
+wd = "C:\\Users\\eturner\\Google Drive\\Small Data Projects\\PR Test\\"
+
+os.chdir(wd)
+
+datasets = wd + "Data\\"
+graphics = wd + "Graphics and Output\\"
+code = wd + "Code\\"
